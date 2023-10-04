d71c646a5b743b1c4e12302520d030a6f03028e0
AlanAvila97 <fidelavila21@gmail.com>
2023-10-04 02:19:59 -0600

    Layouts generales

---
+++ layouts/default.vue
@@ -0,0 +1,17 @@
+<template>
+    <div>
+        <LoadingBar></LoadingBar>        
+        <RedesSociales></RedesSociales>
+        <NavbarView></NavbarView>
+        <slot/>        
+        <FooterView></FooterView>
+    </div>
+</template>
+<script setup>
+
+</script>
+<style scoped>
+.link-active {
+    color: var(--green-logo-color);
+}
+</style>
---
+++ layouts/error.vue
@@ -0,0 +1,19 @@
+<template>
+    <main id="wrapper" class="bg-home">   
+        <section class="pt-md-70 section-404">
+
+            <div class="container-404">
+                <img src="https://canaloncetv.s3.us-east-1.amazonaws.com/REST/data/images/404.webp" width="500" height="500">
+            </div>
+            <div class="">
+                {{ error.statusCode }}
+                {{ error.message }}
+            </div>
+        </section>
+    </main>
+</template>
+
+<script setup>
+    import '@/assets/css/404.css'
+    defineProps({ error: Object });
+</script>
