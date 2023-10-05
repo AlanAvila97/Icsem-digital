86244300ded52beef6183fbcfb2db541f70c92b3
AlanAvila97 <fidelavila21@gmail.com>
2023-10-04 18:40:13 -0600

    composable para preloader

---
+++ composables/actionsLinks.js
@@ -0,0 +1,27 @@
+import {ref, onMounted, onBeforeUpdate, onBeforeUnmount} from 'vue';
+export const actionPreload = () => {
+    const getElement = (element) => {
+        return document.querySelector(element);
+    }
+    const hiddenPreoload = (element, type) => {
+        let body = getElement('body');
+            statusPreload(type);
+            setTimeout(function(){
+                body.classList.remove('overflow-hidden')
+                element.classList.add("d-none");
+            }, 1500); 
+    };
+    const statusPreload = onBeforeUnmount((type) => {
+        const divPreload =  getElement('.'+type);
+        let body = getElement('body');
+            body.classList.add('overflow-hidden')
+        if(divPreload){                
+            divPreload.classList.remove("class","preloader-fade");	        
+            divPreload.classList.remove("class","d-none");	        
+        }
+    });
+    return{
+        hiddenPreoload,
+    }
+    
+}
