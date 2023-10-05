a8dc5268bb310f5d8882f60902673ba3ab2853ee
AlanAvila97 <fidelavila21@gmail.com>
2023-10-05 14:17:30 -0600

    Update nuxt.config.ts

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -51,6 +51,7 @@ export default defineNuxtConfig({
         ],              
         script: [
           { src: 'https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js', tagPosition: 'bodyClose' },
+          { src: 'https://once.fabricaapps.com/js/Isotope/isotope.pkgd.min.js', tagPosition: 'bodyClose' },
         ],
         noscript: [
         ]
