db7786645f283a57deb29cbef903b1b64d34fb87
AlanAvila97 <fidelavila21@gmail.com>
2023-10-05 18:07:19 -0600

    boton para scroll top

--- components/ButtonUp.vue
+++ components/ButtonUp.vue
@@ -1,11 +1,46 @@
 <template>
-    <div class=""></div>
+    <div class="container-btn-up">
+        <button type="button" class="btn-up" 
+                @click="scrollTop"
+                alt="Botón para regresar al top de la pagina">
+            <Icon name="material-symbols:fitbit-arrow-upward"/>
+        </button>
+    </div>
 </template>
 
 <script setup>
-    
+    // Vue
+    import { onMounted } from "vue";
+    /**
+    * @description Funcion que hace un scroll al top de la vista
+    */ 
+    const scrollTop = () => {
+        document.body.scrollTop = 0; 
+        document.documentElement.scrollTop = 0;
+    }
+    onMounted(() => {     
+    }); 
 </script>
 
 <style scoped>
-
+    .container-btn-up{
+        position: relative;
+        padding: 1rem;
+    }
+    .container-btn-up .btn-up{
+        width: 60px;
+        height: 60px;
+        border-radius: 5rem;
+        background: var(--green-logo-color);
+        border: none;
+        box-shadow: 0px 0px 4px 1px #000000b5;
+        position: fixed;
+        bottom: 0.5rem;
+        right: 0.8rem;
+        z-index: 1000;
+    }
+    .container-btn-up .btn-up svg{
+        color: var(--white-icon-color);
+        font-size: 2rem;
+    }
 </style>
