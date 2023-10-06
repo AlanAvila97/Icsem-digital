301d1e5f214abdbaaa9f2add41d4e601aca435c5
AlanAvila97 <fidelavila21@gmail.com>
2023-10-06 13:23:34 -0600

    Funcionalidades para darkmode

--- components/RedesSociales.vue
+++ components/RedesSociales.vue
@@ -4,26 +4,53 @@
             <Icon name="subway:location"/>
             <h2>Ubicacion</h2>
         </div>
-        <div class="social-networks d-flex">
-            <nuxt-link to="/" target="_blank" rel="Redireccion Facebook" aria-label="Redireccion Facebook">
-                <Icon name="fa-brands:facebook-f"/>
-            </nuxt-link>
-            <nuxt-link to="/" target="_blank" rel="Redireccion Twitter" aria-label="Redireccion Twitter">
-                <Icon name="fa-brands:twitter"/>
-            </nuxt-link>
-            <nuxt-link to="/" target="_blank" rel="Redireccion Twitter" aria-label="Redireccion Twitter">
-                <Icon name="fa-brands:instagram"/>                
-            </nuxt-link>
+        <div class="aditionals-social-networks">
+            <div class="social-networks d-flex">
+                <nuxt-link to="/" target="_blank" rel="Redireccion Facebook" aria-label="Redireccion Facebook">
+                    <Icon name="fa-brands:facebook-f"/>
+                </nuxt-link>
+                <nuxt-link to="/" target="_blank" rel="Redireccion Twitter" aria-label="Redireccion Twitter">
+                    <Icon name="fa-brands:twitter"/>
+                </nuxt-link>
+                <nuxt-link to="/" target="_blank" rel="Redireccion Twitter" aria-label="Redireccion Twitter">
+                    <Icon name="fa-brands:instagram"/>                
+                </nuxt-link>
+            </div>
+            <div class="change-mode">
+                <label class="ui-switch" to="checbox-mode">
+                    <input id="checbox-mode" name="checbox-mode" type="checkbox" 
+                           alt="Cambio de color de sitio"
+                           :class="$colorMode.value"
+                           
+                           @click="onClick">
+                    <div class="slider">
+                    <div class="circle"></div>
+                    </div>
+                </label>              
+                <!-- <button @click="onClick" value="light">Light</button>
+                <button @click="onClick" value="dark">Dark</button> -->
+            </div>
         </div>
-        <!-- <div class="change-mode">
-            <label for="check-mode">
-                <input id="check-mode" class="check-mode" type="checkbox">
-                <span class="item-check"></span>
-            </label>
-        </div> -->
     </div>
 </template>
 <script setup>
+    // css 
+    import 'assets/css/switchDarkMode.css'
+    // Vue
+    import { onMounted } from "vue";
+    const colorMode = useColorMode()
+    const statusViewMode = () => {
+        let element = document.querySelector('#checbox-mode');
+        if(colorMode.value === 'dark' || colorMode.value === 'system'){
+            element.setAttribute('checked', '')        
+        }
+    }
+    const onClick = (e) => (
+        colorMode.preference = ( (e.target.checked) ? 'dark' : 'light')
+    )
+    onMounted(() => {
+        statusViewMode()
+    });
 </script>
 <style scoped>
 </style>
--- pages/color.vue
+++ pages/color.vue
@@ -9,8 +9,8 @@
                     <option value="dark">Dark</option>
                 </select>
                 <div >
-                  <button @click="onClick" value="light">Light</button>
-                  <button @click="onClick" value="dark">Dark</button>
+                  <!-- <button @click="onClick" value="light">Light</button>
+                  <button @click="onClick" value="dark">Dark</button> -->
                 </div>
             </div>
         </section>
@@ -34,46 +34,5 @@
   } -->
 
   <style>
-  .dark-mode body {
-    background-color: var(--Outer-color);
-    color: var(--white-icon-color);
-  }
-  .dark-mode body .content-social-networks{    
-    background-color: var(--white-icon-color);
-  }
-  .dark-mode body .content-social-networks *{    
-    color: var(--green-logo-color)!important;
-  }
-  .dark-mode body .header{
-    background-color: var(--trout-color);
-    background-color: var(--Outer-color);
-    background-color: #1e2528;
-  }
-  .dark-mode body .header .container-header .logo picture img{
-    filter: brightness(2) drop-shadow(2px 4px 6px #5f6061);
-  }
-  .dark-mode body .header .container-header .sections-nav .elements-nav li a{
-    color: var(--white-icon-color);
-  }
-  .dark-mode body .header .container-header .sections-nav .elements-nav li .scroll-element{
-    color: var(--white-icon-color);
-  }
-  .dark-mode body .router-link-exact-active{
-    color: var(--green-light-color)!important;
-  }
-  .dark-mode body .section-values .title-value h2,
-  .dark-mode body .section-emails .title-emails h2,
-  .dark-mode body .section-experience .container-experience .title-experience h2,
-  .dark-mode body .section-products-services .container-products-services .title-products-services h2{
-    color: var(--white-icon-color);
-  }
-  .dark-mode body .section-products-services .container-products-services .categories-products .list-categories .item-categorie p{
-    color: var(--white-icon-color);
-  }
-  .dark-mode body .section-products-services .container-products-services .categories-products .list-categories .item-categorie.active p{
-    color: var(--green-light-color);
-  }
-  .dark-mode body .section-product .container-sinopsis-product .sinopsis{
-    box-shadow: 0px 0px 4px 1px #ffffff33;
-  }
+
   </style>
