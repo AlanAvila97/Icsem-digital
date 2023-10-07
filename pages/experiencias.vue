4b5411ceb580bd6ec9298e6a9c5157f544b300f6
AlanAvila97 <fidelavila21@gmail.com>
2023-10-06 19:00:07 -0600

    vista experiencias

---
+++ pages/experiencias.vue
@@ -0,0 +1,23 @@
+<template>
+
+    <main id="wrapper" class=" index">
+        <section class="pt-md-70 section-contact">
+            <div class="container-contact ">
+                <h1>Contactanos</h1>
+                <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Nulla quo ipsa explicabo possimus? Ex numquam qui quam cumque inventore? Nostrum est architecto dolorum nisi quis a sint at quibusdam mollitia.</p>            
+            </div>            
+        </section>
+    </main>
+</template>
+
+<script setup>
+    // Vue
+    import { onMounted } from "vue";
+    // 
+    onMounted(() => {
+    });
+</script>
+
+<style  scoped>
+
+</style>
