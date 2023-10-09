d4b285d4af96eead30af291c2d5c1cb4b7af3614
AlanAvila97 <fidelavila21@gmail.com>
2023-10-09 17:09:49 -0600

    Funcion de inicalizacion de animaciones

--- store/globalData.js
+++ store/globalData.js
@@ -88,6 +88,29 @@ export const dataGlobal = defineStore('dataGlobal', () => {
         var outString = cadena.replace(/[`~!¡@#$%^&*()_|+\=¿?;:'",.<>\{\}\[\]\\\/]/gi, '');
         return outString;
     }
+    /**
+     * @description Función que parsea una cadena a minusculas, elimina caracteres especiales, espacios, acentos
+     * @param cadena Contiene la cadena con caracteres especiales, mayusculas, espacios, acentos
+     * @return {textParser} Retorna la cadena parseada
+    */ 
+   const initializationWow = () => {
+        var wow = new WOW(
+            {
+                boxClass:     'wow',      // animated element css class (default is wow)
+                animateClass: 'animate__animated', // animation css class (default is animated)
+                offset:       0,          // distance to the element when triggering the animation (default is 0)
+                mobile:       true,       // trigger animations on mobile devices (default is true)
+                live:         true,       // act on asynchronously loaded content (default is true)
+                callback:     function(box) {
+                // the callback is fired every time an animation is started
+                // the argument that is passed in is the DOM node being animated
+                },
+                scrollContainer: null,    // optional scroll container selector, otherwise use window,
+                resetAnimation: true,     // reset animation on end (default is true)
+            }
+        );
+        wow.init();
+   }    
     return{
         getElement,
         addAllClass,
@@ -96,6 +119,7 @@ export const dataGlobal = defineStore('dataGlobal', () => {
         getAllElement,
         removeAllClass,
         addClassElement,    
+        initializationWow,
         eliminarCaracteres,
     }
 });
